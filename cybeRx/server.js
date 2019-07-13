db99708a058d6bf323c75de749dfa184a44bf732
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2019-07-13 11:42:01 -0500

    installed watson

--- cybeRx/server.js
+++ cybeRx/server.js
@@ -3,8 +3,39 @@ var app = express();
 
 app.get('/',function(request, response){
 	response.send('Hello, world');
+});
+
+app.get('/', function(request, response){
+	console.log('GET request received at /comments');
+});
+
+app.post('/comments', function(request, response){
+	console.log('POST request received at /comments')
 })
 
 app.listen(3000, function(){
-	console.log("Server is running on port 3000")
-;}) 
+	console.log("Server is running on port 3000");
+}) ;
+
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js');
+					const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+					  version: '2019-07-12',
+					  iam_apikey: 'RMW0D6GsfrKrTlFw_fQR7ZXzWvlN7p-EY2pcQYF8qOUA',
+					  url: 'https://gateway-syd.watsonplatform.net/natural-language-understanding/api'
+					});
+					const analyzeParams = {
+					  'url': 'www.ibm.com',
+					  'features': {
+					    'categories': {
+					      'limit': 3
+					    }
+					  }
+					};
+
+					naturalLanguageUnderstanding.analyze(analyzeParams)
+					  .then(analysisResults => {
+					    console.log(JSON.stringify(analysisResults, null, 2));
+					  })
+					  .catch(err => {
+					    console.log('error:', err);
+					  });
