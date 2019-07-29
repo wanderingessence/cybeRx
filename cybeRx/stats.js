68ceb42e252f8353089322f984e1c02f984e0df5
Chris Chu <42784131+wanderingessence@users.noreply.github.com>
2019-07-29 08:43:21 -0500

    Added the age and gender parameters

--- cybeRx/stats.js
+++ cybeRx/stats.js
@@ -1,7 +1,6 @@
 //extract the username once again
+//tip: Use a default operator 
 var username =  parent.document.URL.substring(parent.document.URL.indexOf('=') + 1, parent.document.URL.length);
-console.log(username);
-
 var firebaseConfig = {
     apiKey: "AIzaSyBPvSqm_MaQhjpK7z0SUbn2ZbxD12MI-9k",
     authDomain: "summerproject1-d1d7c.firebaseapp.com",
@@ -12,15 +11,38 @@ var firebaseConfig = {
     appId: "1:634559623742:web:882b27c70b1247c2"
   };
   firebase.initializeApp(firebaseConfig);
+  var name = "";
+  var age = "";
+  var gender = "";
   var database = firebase.database();
   var ref = database.ref(username);
-  ref.on('value', gotData, errData);
- 
- function gotData(data){
- 	console.log(data.val());
+  var dbCounter = 0;
+ ref.on('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var childData = childSnapshot.val();
+      console.log(childData);
+      name = childData.queue[0].Name;
+      age = Number(childData.queue[1].Age);
+      if(childData.queue[2].Gender == "M"){
+      	gender = "Male";
+      } 
+
+      if(childData.queue[2].Gender == "F"){
+      	gender = "Female";
+      } 
+
+      console.log(age);
+      dbCounter++;
+    });
+   addHTML();
+});
+
+ function addHTML(){
+ 	 document.getElementById("underlineName").innerHTML += "<b><u>" + name + ".</u></b><br><br>";
+ 	 document.getElementById("age").innerHTML += "<b>Age: " + age + "</b><br><br>";
+ 	 document.getElementById("gender").innerHTML += "<b>Gender: " + gender + "</b><br><br>";
  }
 
- function errData(error){
- 	console.log("error!");
- 	console.log(error);
- }
+
+
+
